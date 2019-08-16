549f3db90a336c4562fa6fb425fc1916cb162a30
Graham Fairweather <xotic750@gmail.com>
2019-08-17 01:30:50 +0200

    Maintenance

--- dist/collections-x.js
+++ dist/collections-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-15T20:40:28.201Z",
+  "date": "2019-08-16T23:30:41.679Z",
   "describe": "",
   "description": "ES6 collections fallback library: Map and Set.",
   "file": "collections-x.js",
